4a2767d128b650c9519cc3e56a714e982115eec0
Lakshan Danushka <lakshanx@gmail.com>
2022-06-01 05:28:24 +0530

    Indentation

--- js/carousalMultiSlide.js
+++ js/carousalMultiSlide.js
@@ -5,24 +5,25 @@ $(document).ready(function() {
   if ($carouselMultiSlide.length) {
     $carouselMultiSlide.on('slide.bs.carousel', function(carouselItem) {
 
-    const $carouselInner = $(this).find(".carousel-inner");
-    const $carouselItem = $(carouselItem.relatedTarget);
-    const carouselIndex = $carouselItem.index();
-    const itemsPerSlide = 5; // get this dynamically from col-[number]?
-    const totalItems = $(this).find('.carousel-item').length;
+      const $carouselInner = $(this).find(".carousel-inner");
+      const $carouselItem = $(carouselItem.relatedTarget);
+      const carouselIndex = $carouselItem.index();
+      const itemsPerSlide = 5; // get this dynamically from col-[number]?
+      const totalItems = $(this).find('.carousel-item').length;
 
-    if (carouselIndex >= totalItems - (itemsPerSlide - 1)) {
-      const it = itemsPerSlide - (totalItems - carouselIndex);
-      for (let i = 0; i < it; i++) {
-        //append slides
-        if (carouselItem.direction == "left") {
-          $(this).find('.carousel-item').eq(i).appendTo($carouselInner);
-        } else {
-          $(this).find('.carousel-item').eq(0).appendTo($carouselInner);
+      if (carouselIndex >= totalItems - (itemsPerSlide - 1)) {
+        const it = itemsPerSlide - (totalItems - carouselIndex);
+        for (let i = 0; i < it; i++) {
+          //append slides
+          if (carouselItem.direction == "left") {
+            $(this).find('.carousel-item').eq(i).appendTo($carouselInner);
+          } else {
+            $(this).find('.carousel-item').eq(0).appendTo($carouselInner);
+          }
         }
       }
-    }
-  });
+
+    });
   }
 
   console.log(
